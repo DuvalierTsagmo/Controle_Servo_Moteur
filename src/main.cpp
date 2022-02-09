d1af7ade7cb41efe3570e5a6f1c83cf98b603e9a
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-02-09 11:57:36 -0400

    thinsboard

--- src/main.cpp
+++ src/main.cpp
@@ -8,39 +8,40 @@
 
 #include <Arduino.h>
 #include <Servo.h>
-#include <"WIFIConnector_MKR1000.h">
+#include "WIFIConnector_MKR1000.h"
 #include "MQTTConnector.h"
 
-
-Servo Moteur;  
-
+Servo Moteur;
 
 // Declaration des variables pour le servo moteur
-int Pause = 15; 		
-int ValeurAngle = 0;        
-int ValeurRotation = 0;        
-int DerniereValeurAngle = 0; 
+int Pause = 15;
+int ValeurAngle = 0;
+int ValeurRotation = 0;
+int DerniereValeurAngle = 0;
 
-const int PIN_MOTEUR = 9; 
-const int PIN_CAPTEUR = A5;  		
+const int PIN_MOTEUR = 9;
+const int PIN_CAPTEUR = A5;
 
-void setup() {
-	wifiConnect();                 
-	MQTTConnect();                 
-	Serial.begin(9600);
-	ServoMoteur.attach(PIN_MOTEUR);  
+void setup()
+{
+    wifiConnect();
+    MQTTConnect();
+    Serial.begin(9600);
+    ServoMoteur.attach(PIN_MOTEUR);
 }
 
-void loop() {
-	ValeurRotation = analogRead(PIN_CAPTEUR);
-	ValeurAngle = map(ValeurRotation, 0, 1023, 0, 90);
-	ServoMoteur.write(ValeurAngle);  
-	delay(Pause);
-	if (ValeurAngle != DerniereValeurAngle){
-		
-		appendPayload("ValeurAngle", ValeurAngle);
-		appendPayload("ValeurRotation", ValeurRotation);
-		sendPayload();
-		DerniereValeurAngle = ValeurAngle;
-	}
+void loop()
+{
+    ValeurRotation = analogRead(PIN_CAPTEUR);
+    ValeurAngle = map(ValeurRotation, 0, 1023, 0, 90);
+    ServoMoteur.write(ValeurAngle);
+    delay(Pause);
+    if (ValeurAngle != DerniereValeurAngle)
+    {
+
+        appendPayload("ValeurAngle", ValeurAngle);
+        appendPayload("ValeurRotation", ValeurRotation);
+        sendPayload();
+        DerniereValeurAngle = ValeurAngle;
+    }
 }
